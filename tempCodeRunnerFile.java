65e898b7eeb47cfd4e76c72422fe5519902049ec
Hrishita-Paul <hrishita.paul03@gmail.com>
2023-10-25 21:14:15 +0530

    Initial commit

--- tempCodeRunnerFile.java
+++
@@ -1,4 +0,0 @@
-cashWithdrawal.setCurrentWithdrawal(false);
-        // cashWithdrawal.setSavingsWithdrawal(false);
-        // moneyTransfer.setTransferSuccess(false);
-        // deposit.setDepositSuccess(false);
